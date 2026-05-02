20e6fbd1664e31dc26edbf80294911040e1367f9
LoraDev010 <andreslora010@gmail.com>
2026-05-02 10:02:05 -0500

    feat: configure Firebase project credentials and Remote Config
    - Project: to-do-list-app-58869
    - Remote Config flag: enable_task_priority (default: false)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: 'AIzaSyBkvw36wEKEj5MxAkYTROF7SO7UIaEy89s',
+    authDomain: 'to-do-list-app-58869.firebaseapp.com',
+    projectId: 'to-do-list-app-58869',
+    storageBucket: 'to-do-list-app-58869.firebasestorage.app',
+    messagingSenderId: '69286789074',
+    appId: '1:69286789074:web:3391ac47e9c10d8cfad20b',
+    measurementId: 'G-4B7RK2DF56',
   },
 };
